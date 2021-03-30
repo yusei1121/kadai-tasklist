438ab4438c0d7dbe747eb56acef30d41e9470386
yuusei.yamaguchi <npng_1121@icloud.com>
2021-03-30 04:39:55 +0000

    DRY

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,6 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  
   def index
       @tasks = Task.all
   end
@@ -48,6 +50,10 @@ class TasksController < ApplicationController
   end
   
   private
+  
+  def set_task
+    @task = Task.find(params[:id])
+  end
   
   def task_params
     params.require(:task).permit(:content)
