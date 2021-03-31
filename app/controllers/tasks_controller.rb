68d0688bdacf018d2914454fc70b1a50aade6c20
yuusei.yamaguchi <npng_1121@icloud.com>
2021-03-31 09:14:24 +0000

    fix-kadai

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -28,8 +28,6 @@ class TasksController < ApplicationController
   end
 
   def update
-    @task = Task.find(params[:id])
-
     if @task.update(task_params)
       flash[:success] = 'Task は正常に更新されました'
       redirect_to @task
