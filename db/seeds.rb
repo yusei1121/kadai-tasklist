116572d303db09d0b2ed1c8ac5484c63a3e64576
yuusei.yamaguchi <npng_1121@icloud.com>
2021-03-31 04:01:03 +0000

    db-seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,7 @@
+(1..100).each do |number|
+  Task.create(status: 'test status ' + number.to_s, task: 'test task ' + number.to_s)
+end
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
