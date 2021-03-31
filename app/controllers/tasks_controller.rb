2f2aec4687eccfe00710defa11819439aea8e77f
yuusei.yamaguchi <npng_1121@icloud.com>
2021-03-31 08:38:49 +0000

    validation

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,7 +2,7 @@ class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   
   def index
-      @tasks = Task.all.page(params[:page]).per(10)
+      @tasks = Task.all
   end
 
   def show
